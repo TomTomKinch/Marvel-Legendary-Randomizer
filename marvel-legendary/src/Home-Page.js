42401fd52dce76e05d27feba71df6e6e58f49805
TomTomKinch <kinchthomas@gmail.com>
2021-06-30 13:26:00 -0700

    First Push, Created base react app, added header

---
+++ marvel-legendary/src/Home-Page.js
@@ -0,0 +1,20 @@
+import './Home-Page.css';
+import legendary from './Images/LegendaryLogo.png';
+
+function App() {
+  return (
+    <div className="Home-Page">
+      <header className="Home-Page-Header">
+        <img src={legendary} alt="Legendary" className="Legendary-Logo"/>
+        <p> Randomizer </p>
+      </header>
+      <body>
+        
+
+      </body>
+
+    </div>
+  );
+}
+
+export default App;
