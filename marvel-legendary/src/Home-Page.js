01de14e502e25146c38b051972a471906174ae43
TomTomKinch <kinchthomas@gmail.com>
2021-07-01 15:59:42 -0700

    Can read from mastermind and scheme json w/ hooks

--- marvel-legendary/src/Home-Page.js
+++ marvel-legendary/src/Home-Page.js
@@ -1,17 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
 import './Home-Page.css';
 import legendary from './Images/LegendaryLogo.png';
 
 function App() {
+
+  //Player Count
   const [ playerCount, setPlayerCount ] = useState(0); //Player Count Data
   const { register, handleSubmit } = useForm(); 
-  
   // Sets Player Count Based off Form Selection
   const onSubmit = (data) => {
     console.log(data[0]);
     setPlayerCount(data[0]);
   }
+
+  //Scheme
+  const [ scheme, setScheme ] = useState([{
+    "ID": "",
+    "name": "",
+    "leads": "",
+    "leadsType": "",
+    "set": ""
+  }]);
+  useEffect(() => {
+    fetch('./JSON Files/scheme.json')
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      setScheme(data);
+      //console.log(scheme[0].name);
+    })
+  },[]);
+
+  //MasterMind
+  const [ masterMind, setMasterMind ] = useState([{
+    "ID": "",
+    "name": "",
+    "hp": "",
+    "masterStrike": "",
+    "leads": "",
+    "leadsType": "",
+    "set": ""
+  }]); 
+  useEffect(() => {
+    fetch('./JSON Files/masterMind.json')
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      setMasterMind(data);
+      // console.log(masterMind[0].name);
+    })
+  }, []);
 
   return (
     <div className="Home-Page">
@@ -34,6 +73,18 @@ function App() {
         <input type="submit"/>
       </form>
       <p>Number of Players: {playerCount}</p>
+      <p>-----------------------------------------------------</p>
+      {/* MasterMind Info */}
+      <h2>MasterMind Data: </h2>
+      <p>MasterMind: {masterMind[0].name} </p>
+      <p>HP: {masterMind[0].hp} </p>
+      <p>MasterStrike: {masterMind[0].masterStrike} </p>
+      <p>Set: {masterMind[0].set} </p>
+
+      {/* Scheme Info */}
+      <h2>Scheme Data: </h2>
+      <p>Scheme: {scheme[0].name} </p>
+      <p>Set: {scheme[0].set}</p>
     </div>
   );
 }
